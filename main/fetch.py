27b09682f68465501632ce3492f2cbe190b30f1c
Vishal Venkatraman <ihandsomeguy97@gmail.com>
2021-09-19 16:29:26 +0800

    API Call Documentation

--- main/fetch.py
+++ main/fetch.py
@@ -1,24 +1,31 @@
 import os
 from requests import get
+# Optional - loads API key stored in a separate .env file. For security.
 from dotenv import load_dotenv
 
-if load_dotenv():
+def getPM25():
 
+    # Variables for constructing the API Call
     path = "https://api.waqi.info/feed/"
     city = "hongkong/"
     apiToken = os.getenv("pm25_api_token")
 
-    # Get data
+    # Get data using the get() function. It needs a completed API Request URL
     response = get(path + city + "?token=" + apiToken)
 
+    # response.josn() provides the result of your API call. 
     payload = response.json()["data"]
 
+    # These variables break down the JSON data and gathers the important/useful into. 
     location = payload["city"]["name"]
     timeStamp = payload["time"]["s"]
     pm25Rating = payload["iaqi"]["pm25"]["v"]
 
+    # Print the result of the data to ensure that the API call worked. 
     print("Location: " + location)
-
     print("Time Stamp: " + timeStamp)
-
     print("PM2.5 Rating: " + str(pm25Rating) + "\n")
+
+# If the .env file can be loaded, make the API calls
+if load_dotenv():
+    getPM25()
